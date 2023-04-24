ff8dc4d92814f902647931af83de9903777bf86b
Kapil Chaudhary <108571992+Kapil685838@users.noreply.github.com>
2023-04-24 14:53:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,9 +2,10 @@ const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 
 function hasKey(key) {
   //   write your code here
-	return Object.keys(sampleObject).includes(key); 
+	let arrOfKeys = Object.keys(sampleObject); //["red", "green", "white"];
+	return arrOfKeys.includes(key); 
 }
 
 // Do not change the code below
-const key = prompt("Enter Key.");
+// const key = prompt("Enter Key.");
 alert(hasKey(key));
